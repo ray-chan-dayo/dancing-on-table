4649ccba0dcfa55b8d8feb501b34d4fe5a45ace3
ray-chan-dayo <64572413+ray-chan-dayo@users.noreply.github.com>
2024-02-15 01:29:25 +0900

    vrm works with ar.js

--- main.js
+++ main.js
@@ -29,7 +29,19 @@ var onRenderFcts = [];
 var arToolkitContext, arMarkerControls;
 // init scene and camera
 var scene = new THREE.Scene();
+
 //load vrm-mixamo-three.js
+const defaultModelUrl = './humanoidAnimation/rei_siro8.vrm';
+const mixamoFbxUrl = './humanoidAnimation/Dancing.fbx'
+
+// gltf and vrm
+let currentVrm = undefined;
+let currentAnimationUrl = undefined;
+let currentMixer = undefined;
+
+const helperRoot = new THREE.Group();
+helperRoot.renderOrder = 10000;
+scene.add( helperRoot );
 
 //////////////////////////////////////////////////////////////////////////////////
 //		Initialize a basic camera
@@ -131,29 +143,115 @@ onRenderFcts.push(function () {
     }
     arToolkitContext.update(arToolkitSource.domElement)
     // update scene.visible if the marker is seen
-    scene.visible = camera.visible
+      scene.visible = camera.visible
 })
 //////////////////////////////////////////////////////////////////////////////////
 //		add an object in the scene
 //////////////////////////////////////////////////////////////////////////////////
 // add a torus knot
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshNormalMaterial({
-    transparent: true,
-    opacity: 0.5,
-    side: THREE.DoubleSide
-});
-var mesh = new THREE.Mesh(geometry, material);
-mesh.position.y = geometry.parameters.height / 2
-scene.add(mesh);
-var geometry = new THREE.TorusKnotGeometry(0.3, 0.1, 64, 16);
-var material = new THREE.MeshNormalMaterial();
-var mesh = new THREE.Mesh(geometry, material);
-mesh.position.y = 0.5
-scene.add(mesh);
-onRenderFcts.push(function (delta) {
-    mesh.rotation.x += Math.PI * delta
-})
+// var geometry = new THREE.BoxGeometry(1, 1, 1);
+// var material = new THREE.MeshNormalMaterial({
+//     transparent: true,
+//     opacity: 0.5,
+//     side: THREE.DoubleSide
+// });
+// var mesh = new THREE.Mesh(geometry, material);
+// mesh.position.y = geometry.parameters.height / 2
+// scene.add(mesh);
+// var geometry = new THREE.TorusKnotGeometry(0.3, 0.1, 64, 16);
+// var material = new THREE.MeshNormalMaterial();
+// var mesh = new THREE.Mesh(geometry, material);
+// mesh.position.y = 0.5
+// scene.add(mesh);
+// onRenderFcts.push(function (delta) {
+//     mesh.rotation.x += Math.PI * delta
+// })
+//////////////////////////////////////////////////////////////////////////////////
+//		load vrm and fbx
+//////////////////////////////////////////////////////////////////////////////////
+function loadVRM( modelUrl ) {
+
+	const loader = new GLTFLoader();
+	loader.crossOrigin = 'anonymous';
+
+	// helperRoot.clear();
+
+	loader.register( ( parser ) => {
+
+		return new VRMLoaderPlugin( parser, { helperRoot: helperRoot, autoUpdateHumanBones: true } );
+
+	} );
+
+	loader.load(
+		// URL of the VRM you want to load
+		modelUrl,
+
+		// called when the resource is loaded
+		( gltf ) => {
+
+			const vrm = gltf.userData.vrm;
+
+			if ( currentVrm ) {
+
+				scene.remove( currentVrm.scene );
+
+				VRMUtils.deepDispose( currentVrm.scene );
+
+			}
+
+			// put the model to the scene
+			currentVrm = vrm;
+			scene.add( vrm.scene );
+
+			// Disable frustum culling
+			vrm.scene.traverse( ( obj ) => {
+
+				obj.frustumCulled = false;
+
+			} );
+
+			if ( mixamoFbxUrl ) {
+
+				loadFBX( mixamoFbxUrl );
+
+			}
+
+			// rotate if the VRM is VRM0.0
+			VRMUtils.rotateVRM0( vrm );
+
+			console.log( vrm );
+
+		},
+
+		// called while loading is progressing
+		( progress ) => console.log( 'Loading model...', 100.0 * ( progress.loaded / progress.total ), '%' ),
+
+		// called when loading has errors
+		( error ) => console.error( error ),
+	);
+
+}
+
+loadVRM( defaultModelUrl );
+
+// mixamo animation
+function loadFBX( animationUrl ) {
+
+	currentAnimationUrl = animationUrl;
+
+	// create AnimationMixer for VRM
+	currentMixer = new THREE.AnimationMixer( currentVrm.scene );
+
+	// Load animation
+	loadMixamoAnimation( animationUrl, currentVrm ).then( ( clip ) => {
+
+		// Apply the loaded animation to mixer and play
+		currentMixer.clipAction( clip ).play();
+		currentMixer.timeScale = 1.0;
+
+	} );
+
+}
 //////////////////////////////////////////////////////////////////////////////////
 //		render the whole thing on the page
 //////////////////////////////////////////////////////////////////////////////////
@@ -162,10 +260,7 @@ onRenderFcts.push(function () {
     renderer.render(scene, camera);
 })
 //deltaを取得するためclockを定義
-// const clock = new THREE.Clock();
-// const dara = initvrmthree( modelUrl )
-// const currentMixer = initvrmthree( modelUrl )[0]
-// const currentVrm = initvrmthree( modelUrl )[0]
+const clock = new THREE.Clock();
 // run the rendering loop
 var lastTimeMsec = null
 requestAnimationFrame(function animate(nowMsec) {
@@ -179,4 +274,22 @@ requestAnimationFrame(function animate(nowMsec) {
     onRenderFcts.forEach(function (onRenderFct) {
         onRenderFct(deltaMsec / 1000, nowMsec / 1000)
     })
+
+	const deltaTime = clock.getDelta();
+
+	// if animation is loaded
+	if ( currentMixer ) {
+
+		// update the animation
+		currentMixer.update( deltaTime );
+
+	}
+
+	if ( currentVrm ) {
+
+		currentVrm.update( deltaTime );
+
+	}
+
+	renderer.render( scene, camera );
 })
